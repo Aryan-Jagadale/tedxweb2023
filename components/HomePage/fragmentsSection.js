a1ad3bbe18ec1557c8ca87f00505520dbcdf9243
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-03-30 15:06:35 +0530

    Fragments

--- components/HomePage/fragmentsSection.js
+++ components/HomePage/fragmentsSection.js
@@ -1,11 +1,9 @@
 export default function FragmentsSection() {
   return (
-    <div className="max-w-6xl mx-auto py-12 lg:py-16 lg:flex lg:items-center lg:justify-between">
-      <div className="w-full lg:w-1/2 px-4 lg:py-12 my-4">
-        <h2 className="w-full font-extrabold text-7xl lg:text-8xl xl:text-9xl text-white leading-tight heading text-center">
-          FRAG
-          <br />
-          MENTS
+    <div className={fragmentStyle.fragmentSection}>
+      <div className=''>
+        <h2 className={'heading'} id={fragmentStyle.fragmentHeading}>
+        FRAG<br />MENTS
         </h2>
       </div>
       <div className="w-full lg:w-1/2 px-4 lg:py-12 my-4 text-white text-justify font-medium text-xl md:text-2xl">
