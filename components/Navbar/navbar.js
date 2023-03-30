ab30a7eb5b14442ed98503eafd28e5444a2acdde
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-03-30 14:00:33 +0000

    navbar done with animation

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import navStyle from '../../styles/navbarNew.module.scss'
 
@@ -7,8 +8,27 @@ import NavLinks from './navLinks'
 import tedxlogo from '../../assets/icons/logo-white.png'
 
 export default function Navbar() {
+
+  const [showDiv, setShowDiv] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 300) {
+        setShowDiv(true);
+      } else {
+        setShowDiv(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={navStyle.navbar}>
+    <div className={`${navStyle['scroll-div']} ${showDiv ? navStyle.show : ''}`} id={navStyle.navbar}>
       <SocialAccount />
 
       <div className={navStyle.logo}>
