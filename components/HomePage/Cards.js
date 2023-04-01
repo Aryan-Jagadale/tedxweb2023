6c726fe9a40a90b5b730bfb5ed28bf9084b3e710
Tilak Dave <davetilak003@gmail.com>
2023-04-01 16:28:05 +0530

    horizontal scroll

--- components/HomePage/Cards.js
+++ components/HomePage/Cards.js
@@ -5,20 +5,20 @@ export default function Cards() {
     <div className="">
       <div className="max-w-6xl mx-auto py-12 lg:py-16 lg:flex lg:items-center lg:justify-between px-6">
         <div className="w-full lg:w-1/2 px-8 lg:px-12 py-8 lg:py-12 lg:mr-4 bg-[#161616] rounded-[40px] shadow-md">
-          <h2 className="text-3xl heading text-center font-extrabold tracking-tight text-white sm:text-5xl">
+          <h2 className="text-3xl heading text-center font-extrabold tracking-tight text-white md:text-5xl">
             About Us
           </h2>
-          <p className="mt-12 text-[28px] text-white text-justify">
+          <p className="mt-12 text-xl md:text-3xl text-white text-justify">
             In the spirit of Ideas Worth Spreading, we, at TEDxVITPune, solely
             believe in the power of ideas to change attitudes, lives, and
             ultimately, the world at large. We're bringing together people from
             all different walks of life to come, confer and confabulate ideas in
             whole different dimensions.
           </p>
-          <div className="mt-16 flex justify-end">
+          <div className="mt-8 lg:mt-16 flex justify-end">
             <Link href="/about">
-              <div className="bg-[#dd1313] h-16 w-16 rounded-full flex justify-center items-center cursor-pointer">
-                <img className="w-12 h-12" src="/rc.svg" alt="chevron-right" />
+              <div className="bg-[#dd1313] h-12 w-12 md:h-16 md:w-16 rounded-full flex justify-center items-center cursor-pointer">
+                <img className="w-6 h-6 md:w-12 md:h-12" src="/rc.svg" alt="chevron-right" />
               </div>
             </Link>
           </div>
@@ -26,33 +26,33 @@ export default function Cards() {
         <div className="w-full lg:w-1/2 lg:ml-4">
           <div className="w-full px-8 lg:px-12 py-12 lg:py-16 my-8 bg-[#161616] rounded-[40px] shadow-md">
             <div className="flex justify-between items-center">
-              <h2 className="text-3xl heading font-extrabold tracking-tight text-white sm:text-5xl">
+              <h2 className="text-3xl heading font-extrabold tracking-tight text-white md:text-5xl">
                 Lookbook
               </h2>
               <Link href="/lbv2">
-              <div className="bg-[#dd1313] h-16 w-16 rounded-full flex justify-center items-center cursor-pointer">
-                <img className="w-12 h-12" src="/rc.svg" alt="chevron-right" />
+              <div className="bg-[#dd1313] h-12 w-12 md:h-16 md:w-16 rounded-full flex justify-center items-center cursor-pointer">
+                <img className="w-6 h-6 md:w-12 md:h-12" src="/rc.svg" alt="chevron-right" />
               </div>
               </Link>
             </div>
           </div>
           <div className="w-full px-8 lg:px-12 py-12 lg:py-16 my-8 bg-[#161616] rounded-[40px] shadow-md">
             <div className="flex justify-between items-center">
-              <h2 className="text-3xl heading font-extrabold tracking-tight text-white sm:text-5xl">
+              <h2 className="text-3xl heading font-extrabold tracking-tight text-white md:text-5xl">
                 Endeavours
               </h2>
-              <div className="bg-[#dd1313] h-16 w-16 rounded-full flex justify-center items-center cursor-pointer">
-                <img className="w-12 h-12" src="/rc.svg" alt="chevron-right" />
+              <div className="bg-[#dd1313] h-12 w-12 md:h-16 md:w-16 rounded-full flex justify-center items-center cursor-pointer">
+                <img className="w-6 h-6 md:w-12 md:h-12" src="/rc.svg" alt="chevron-right" />
               </div>
             </div>
           </div>
           <div className="w-full px-8 lg:px-12 py-12 lg:py-16 my-8 bg-[#161616] rounded-[40px] shadow-md">
             <div className="flex justify-between items-center">
-              <h2 className="text-3xl heading font-extrabold tracking-tight text-white sm:text-5xl">
+              <h2 className="text-3xl heading font-extrabold tracking-tight text-white md:text-5xl">
                 NewsLetter
               </h2>
-              <div className="bg-[#dd1313] h-16 w-16 rounded-full flex justify-center items-center cursor-pointer">
-                <img className="w-12 h-12" src="/rc.svg" alt="chevron-right" />
+              <div className="bg-[#dd1313] h-12 w-12 md:h-16 md:w-16 rounded-full flex justify-center items-center cursor-pointer">
+                <img className="w-6 h-6 md:w-12 md:h-12" src="/rc.svg" alt="chevron-right" />
               </div>
             </div>
           </div>
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import cn from 'classnames';
+import styles from '@/styles/about.module.scss';
+
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={cn(styles.horizontalScrollWrapper, styles.squares, 'text-white')}>
+      <div>item 1</div>
+      <div>item 2</div>
+      <div>item 3</div>
     </div>
-  )
-}
+  );
+};
