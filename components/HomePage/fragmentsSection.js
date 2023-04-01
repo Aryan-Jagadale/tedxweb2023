ab55a8ce881fbe158b6117f64f550b9c1873001c
Tilak Dave <davetilak003@gmail.com>
2023-04-01 14:55:25 +0530

    navbar fixed, scrollbar styling

--- components/HomePage/fragmentsSection.js
+++ components/HomePage/fragmentsSection.js
@@ -1,24 +1,30 @@
 export default function FragmentsSection() {
   return (
     <div className="max-w-6xl mx-auto py-12 lg:py-16 lg:flex lg:items-center lg:justify-between">
-    <div className="w-full lg:w-1/2 px-4 lg:px-8 lg:py-12 my-4">
-      <h2 className="w-full font-extrabold text-8xl  xl:text-9xl text-white leading-tight heading text-center lg:text-left">
-        FRAG
-        <br />
-        MENTS
-      </h2>
+      <div className="w-full lg:w-1/2 px-4 lg:py-12 my-4">
+        <h2 className="w-full font-extrabold text-8xl xl:text-9xl text-white leading-tight heading text-center">
+          FRAG
+          <br />
+          MENTS
+        </h2>
+      </div>
+      <div className="w-full lg:w-1/2 px-4 lg:py-12 my-4 text-white text-justify font-medium text-2xl">
+        <p>
+          All the strum and drag in our heads draws down to introspecting-
+          engulfed by turmoil. From raging uproars to cheerful celebrations,
+          spiralling times to exciting unknowns, we've been thriving amidst the
+          chaos.
+          <br />
+          We often wonder, will we ever seek answers? Will the chaos make
+          sense? Now is the time we inch closer to our reality by being grateful
+          for the minute moments, and not losing a sense of empathy, triumphing in
+          our trials- through the balance that comes from within.
+        </p>
+        <div className="w-full flex justify-center items-center">
+          <button className="mt-8 px-6 py-2 bg-red-600 rounded text-3xl text-white">Button</button>
+        </div>
+      </div>
     </div>
-    <div className="w-full lg:w-1/2 px-5 lg:px-3 lg:py-12 my-4 text-white text-center lg:text-start font-medium text-2xl">
-      All the strum and drag in our heads draws down to introspecting-
-      engulfed by turmoil. From raging uproars to cheerful celebrations,
-      spiralling times to exciting unknowns, we've been thriving amidst the
-      chaos. We often wonder, will we ever seek answers? Will the chaos make
-      sense? Now is the time we inch closer to our reality by being grateful
-      for the minute moments, and not losing a sense of empathy, triumphing in
-      our trials- through the balance that comes from within. <br />
-      <button className="mt-4 px-4 py-2 bg-red-700 rounded text-white">Button</button>
-    </div>
-  </div>
-  
+
   )
 }
