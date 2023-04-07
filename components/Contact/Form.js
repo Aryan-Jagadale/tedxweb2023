3ad2133ec8a952bfdad466569d06ba6f51b42968
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-04-07 11:21:45 +0530

    Working on Transition Effects

---
+++ components/Contact/Form.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Form = ({ number, question, placeHolder,contactDetails }) => {
+  return (
+    <section className="w-full flex flex-col md:flex-row justify-center gap-7">
+
+      <div className="w-full md:w-[70%]">
+        <section className="border-t-2 border-[#6A6A6A] py-10 md:py-20">
+          <div className="w-full flex">
+            <section className="w-[10%] text-[#505050] font-thin  tracking-wide text-xl hidden md:block">
+              {number}
+            </section>
+            <section className="w-[90%]  ">
+              <h3 className="font-bold tracking-wide text-2xl">{question}</h3>
+              <textarea
+                type="text"
+                placeholder={placeHolder}
+                className="outline-none text-lg tracking-wider text-[#6A6A6A] mt-7 mb-0 md:my-7 w-[90%] md:h-full overflow-hidden"
+              />
+            </section>
+          </div>
+        </section>
+      </div>
+
+      <div className="w-full md:w-[30%] hidden md:block">
+        {contactDetails}
+        
+      </div>
+
+    </section>
+  )
+}
+
+export default Form
---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import ContactForm from '@/components/Contact/ContactForm'
+import ContactHeader from '@/components/Contact/ContactHeader'
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <ContactHeader />
+      <ContactForm />
+    </div>
+  )
+}
+
+export default contact
