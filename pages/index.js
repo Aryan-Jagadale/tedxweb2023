5830ba842c901a6c0f0d51406fd183858d57f72b
Tilak Dave <davetilak003@gmail.com>
2023-04-08 15:00:15 +0530

    timeline update

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
       >
         <Hero />
         <FragmentsSection />
-        <Cards />
         <TimeLineSection/>
+        <Cards />
       </main>
     </>
   )
