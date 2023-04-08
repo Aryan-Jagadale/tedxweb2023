17f35ee256996db9c3626b6f8745829ed3118200
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-04-08 11:10:02 +0530

    Timeline done

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
+import { AnimatePresence } from 'framer-motion'
 import Layout from '@/components/layout'
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} />
+      </AnimatePresence>
     </Layout>
   )
 }
