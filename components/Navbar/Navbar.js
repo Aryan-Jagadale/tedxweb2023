f389e957e690855c489a75e9cdfb9d1a9cc78946
Aryan-Jagadale <aryansjagadale@gmail.com>
2023-04-09 18:42:07 +0530

    Done footer

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,56 +1,81 @@
-import Link from "next/link"
-import MobileMenu from "./MobileMenu"
-import cn from 'classnames';
-import styles from '../../styles/mobile-menu.module.scss';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Link from 'next/link'
+import MobileMenu from './MobileMenu'
+import cn from 'classnames'
+import styles from '../../styles/mobile-menu.module.scss'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
-  const router = useRouter();
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+  const router = useRouter()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   function toggleMenu() {
     if (isMenuOpen) {
-      setIsMenuOpen(false);
+      setIsMenuOpen(false)
     } else {
-      setIsMenuOpen(true);
+      setIsMenuOpen(true)
     }
   }
 
   useEffect(() => {
     if (isMenuOpen) {
-      setIsMenuOpen(!isMenuOpen);
+      setIsMenuOpen(!isMenuOpen)
     }
-  }, [ router.asPath ]);
+  }, [router.asPath])
 
   return (
     <div className="w-full sticky top-0 z-30">
       <div className="flex flex-col justify-center w-full bg-black/30 backdrop-blur-xl border-0 border-b-4 border-[#161616]">
         <nav className="flex items-center justify-between w-full relative md:max-w-8xl mx-auto py-2 px-6">
-          <div className='hidden md:flex md:flex-row'>
+          <div className="hidden md:flex md:flex-row">
             <a href="https://instagram.com/tedxvitpune" target="_blank">
-              <img src='/icons/instagram.svg' className='w-12 h-12' alt="instagram" />
+              <img
+                src="/icons/instagram.svg"
+                className="w-12 h-12"
+                alt="instagram"
+              />
             </a>
             <a href="https://twitter.com/tedxvitpune" target="_blank">
-              <img src='/icons/twitter.svg' className='w-12 h-12' alt="twitter" />
+              <img
+                src="/icons/twitter.svg"
+                className="w-12 h-12"
+                alt="twitter"
+              />
             </a>
             <a href="https://facebook.com/tedxvitpune" target="_blank">
-              <img src='/icons/facebook.svg' className='w-12 h-12' alt="facebook" />
+              <img
+                src="/icons/facebook.svg"
+                className="w-12 h-12"
+                alt="facebook"
+              />
             </a>
-            <a href="https://www.linkedin.com/company/tedxvitpune/mycompany/" target="_blank">
-              <img src='/icons/linkedin.svg' className='w-12 h-12' alt="linkedin" />
+            <a
+              href="https://www.linkedin.com/company/tedxvitpune/mycompany/"
+              target="_blank"
+            >
+              <img
+                src="/icons/linkedin.svg"
+                className="w-12 h-12"
+                alt="linkedin"
+              />
             </a>
           </div>
           <div>
             <Link href="/">
-              <img src='/logo.svg' className="w-3/5 md:ml-24" alt="tedxlogo" />
+              <img src="/logo.svg" className="w-3/5 md:ml-24" alt="tedxlogo" />
             </Link>
           </div>
           <div className="flex flex-row justify-end items-center text-md font-bold">
-            <Link href="/about" className="text-white mr-8 hidden hover:underline md:block z-50">
+            <Link
+              href="/about"
+              className="text-white mr-8 hidden hover:underline md:block z-50"
+            >
               ABOUT
             </Link>
-            <Link href="/lookbook" className="text-white mr-8 hidden hover:underline md:block z-50">
+            <Link
+              href="/lookbook"
+              className="text-white mr-8 hidden hover:underline md:block z-50"
+            >
               LOOKBOOK
             </Link>
             <button
@@ -95,7 +120,7 @@ function MenuIcon(props) {
         strokeLinejoin="round"
       />
     </svg>
-  );
+  )
 }
 
 function CrossIcon(props) {
@@ -116,6 +141,5 @@ function CrossIcon(props) {
       <path d="M18 6L6 18" />
       <path d="M6 6l12 12" />
     </svg>
-  );
+  )
 }
-
