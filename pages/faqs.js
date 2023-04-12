9fad6c4b28cfb0b28f689b046f97a3a9843a49cd
Tilak Dave <davetilak003@gmail.com>
2023-04-12 16:08:14 +0530

    updates

---
+++ pages/faqs.js
@@ -0,0 +1,11 @@
+import FAQsHeader from '@/components/FAQs/FAQsHeader'
+import FAQsSection from '@/components/FAQs/FAQsSection'
+
+export default function FAQs () {
+    return (
+        <div>
+            <FAQsHeader />
+            <FAQsSection />
+        </div>
+    )
+}
