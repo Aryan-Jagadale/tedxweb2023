bad5b374dfaa708505d7e037153c55be36f6c473
Tilak Dave <davetilak003@gmail.com>
2023-04-14 18:23:51 +0530

    hero video

--- components/HomePage/Hero.js
+++ components/HomePage/Hero.js
@@ -2,11 +2,9 @@ export default function Hero() {
   return (
     <div className="bg-black flex justify-center items-center relative"
     >
-      <video src="hero2.mp4" autoPlay muted loop className="xl:h-[90vh] w-screen xl:object-cover">
+      <video src="hero.m4v" autoPlay muted loop className="xl:h-[90vh] w-screen xl:object-cover">
       </video>
-
       {/* <img src="stamp.png" className="absolute bottom-0 right-0 w-16 md:w-24 xl:w-32" /> */}
-
     </div>
   )
 }
