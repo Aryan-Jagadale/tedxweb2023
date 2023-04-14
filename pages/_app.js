71a39c21515ff91147a00843647b7f791ebc665b
Tilak Dave <davetilak003@gmail.com>
2023-04-14 20:04:47 +0530

    google analytcics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,47 @@
 import { AnimatePresence } from 'framer-motion'
 import Layout from '@/components/layout'
 import '@/styles/globals.scss'
+import { useEffect } from 'react'
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as gtag from '@/components/gtag';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `
+        }}
+      />
     
         <AnimatePresence
           mode="wait"
